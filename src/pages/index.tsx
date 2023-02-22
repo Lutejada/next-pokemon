1b3c7520f7afbcd53d314077693692704106d18d
Luis Tejada <luetejava@gmail.com>
2023-02-22 18:13:37 -0500

    se instala axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,35 @@
-import { Button } from "@nextui-org/react";
+import { pokeApi } from 'api';
+import { PokemonListresponse } from 'interfaces';
+import { GetStaticProps } from 'next'
+
+
+
 import { Layouts } from '../components/layouts/';
 
 
-export default function HomePage() {
+export default function HomePage(props) {
+
+  console.log({props})
   return (
     <Layouts title= 'listado de pokemones'>
-      <h1>Hola Mundo</h1>
-      <Button color='gradient'>Hola Mundo</Button>
+      <ul>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+      </ul>
     </Layouts>
   )
+}
+
+
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await pokeApi.get<PokemonListresponse>('/pokemon?limit=151')
+
+  
+
+  return {
+    props: {
+      pokemons: data.results
+    }
+  }
 }
