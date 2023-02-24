cade67607e4d23c228067ef474ee9face267d1e3
Luis Tejada <luetejava@gmail.com>
2023-02-23 21:56:46 -0500

    hay un error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,47 @@
-import { pokeApi } from 'api';
-import { PokemonListresponse } from 'interfaces';
-import { GetStaticProps } from 'next'
-
-
+import { pokeApi } from '../../api';
+import { PokemonListresponse} from 'interfaces';
+import { GetStaticProps, NextPage } from 'next'
 
 import { Layouts } from '../components/layouts/';
+import { SmallPokemon } from '../../interfaces';
+
+interface props {
+  pokemons: SmallPokemon[];
+}
 
 
-export default function HomePage(props) {
+ const HomePage: NextPage<props> = ({pokemons}) => {
 
-  console.log({props})
+  console.log(pokemons)
+
+  
+
   return (
-    <Layouts title= 'listado de pokemones'>
+    <Layouts title = 'listado de pokemones'>
       <ul>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
+        {
+          pokemons.map( ( { id, name} ))=>(
+            <li key = {id}>
+               #{id} - {name} 
+            </li>
+          )
+        }
       </ul>
     </Layouts>
   )
 }
 
-
+export default HomePage;
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const { data } = await pokeApi.get<PokemonListresponse>('/pokemon?limit=151')
 
-  
+  const pokemons: SmallPokemon[]= data.results.map( (poke, i) => ({
+    ...poke,
+    id: i + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${i + 1}.svg`
+
+  }))
 
   return {
     props: {
